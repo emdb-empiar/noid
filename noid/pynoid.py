7033b14001a28e1c35d94a47b46690eb0a65ff45
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2021-08-17 16:21:15 +0100

    legibility: renaming methods, comments, formatting PEP8

--- noid/pynoid.py
+++ noid/pynoid.py
@@ -1,17 +1,27 @@
+import os
+import sys
 from random import randint
 
-DIGIT = ['0','1','2','3','4','5','6','7','8','9']
-XDIGIT = DIGIT + ['b','c','d','f','g','h','j','k','m','n','p','q','r','s','t','v','w','x','z']
+DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  # 10
+#
+XDIGIT = DIGIT + ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w',
+                  'x', 'y', 'z'] + \
+         ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+          'Z']
 GENTYPES = ['r', 's', 'z']
 DIGTYPES = ['d', 'e']
-#SHORT = '.shrt.'
+CHECKDIG = ['k']
+# SHORT = '.shrt.'
 SHORT = ''
 
-def mint(template='zek', n=None, scheme=None, naa=None):
-    ''' Mint identifiers according to template with a prefix of scheme + naa.
 
-    Template is of form [mask] or [prefix].[mask] where prefix is any URI-safe string and mask is a string of any combination 'e' and 'd', optionally beginning with a mint order indicator ('r'|'s'|'z') and/or ending with a checkdigit ('k'):
-    
+def mint(template='zek', n=None, scheme=None, naa=None):
+    """ Mint identifiers according to template with a prefix of scheme + naa.
+
+    Template is of form [mask] or [prefix].[mask] where prefix is any URI-safe string and mask is a string of any
+    combination 'e' and 'd', optionally beginning with a mint order indicator ('r'|'s'|'z') and/or ending
+    with a checkdigit ('k'):
+
     Example Templates:
     d      : 0, 1, 2, 3
     zek    : 00, xt, 3f0, 338bh
@@ -21,10 +31,10 @@ def mint(template='zek', n=None, scheme=None, naa=None):
 
     The result is appended to the scheme and naa as follows: scheme + naa + '/' + [id].
 
-    There is no checking to ensure ids are not reminted. Instead, minting can be controlled by supplying a (int) value for 'n'. It is possible to implement ordered or random minting from available ids by manipulnating this number from another program. If no 'n' is given, minting is random from within the namespace. An indicator is added between '/' and [id] to mark these ids as for short term testing only. An override may be added later to accommodate applications which don't mind getting used ids. 
+    There is no checking to ensure ids are not reminted. Instead, minting can be controlled by supplying a (int) value for 'n'. It is possible to implement ordered or random minting from available ids by manipulnating this number from another program. If no 'n' is given, minting is random from within the namespace. An indicator is added between '/' and [id] to mark these ids as for short term testing only. An override may be added later to accommodate applications which don't mind getting used ids.
 nn
     A note about 'r', 's', and 'z': 'z' indicates that a namespace should expand on its first element to accommodate any 'n' value (eg. 'de' becomes 'dde' then 'ddde' as numbers get larger). That expansion can be handled by this method. 'r' and 's' (typically meaning 'random' and 'sequential') are recognized as valid values, but ignored and must be implemented elsewhere.
-    '''
+    """
 
     if '.' in template:
         prefix, mask = template.rsplit('.', 1)
@@ -32,23 +42,24 @@ nn
         mask = template
         prefix = ''
 
+    # validate the mask
     try:
-        __validateMask(mask)
+        _validate_mask(mask)
     except:
         raise
 
-    if n == None:
-        if mask[0] in (GENTYPES):
+    if n is None:
+        if mask[0] in GENTYPES:
             mask = mask[1:]
         # If we hit this point, this is a random (and therefore, short-term) identifier. 
         prefix = SHORT + prefix
-        n = randint(0, __getTotal(mask) -1)
+        n = randint(0, _get_total(mask) - 1)
 
-    noid = prefix + __n2xdig(n, mask)
+    noid = f"{prefix}.{generate_noid(n, mask)}"
     if naa:
         noid = naa.strip('/') + '/' + noid
     if template[-1] == 'k':
-        noid += __checkdigit(noid)
+        noid += calculate_check_digit(noid)
     if scheme:
         noid = scheme + noid
 
@@ -56,69 +67,71 @@ nn
 
 
 def validate(s):
-    '''Checks if the final character is a valid checkdigit for the id. Will fail for ids with no checkdigit.
+    """Checks if the final character is a valid checkdigit for the id. Will fail for ids with no checkdigit.
 
     This will also attempt to strip scheme strings (eg. 'doi:', 'ark:/') from the beginning of the string. This feature is limited, however, so if you haven't tested with your particular namespace, it's best to pass in ids with that data removed.
 
     Returns True on success, ValidationError on failure.
-    '''
-    if not __checkdigit(s[0:-1]) == s[-1]:
-        raise ValidationError("Noid check character '" + s[-1] + "' doesn't match up for '" + s + "'.")
-    return True
+    """
+    return calculate_check_digit(s[0:-1]) == s[-1]
 
 
-def __n2xdig(n, mask):
-    req = n
-    xdig = ''
-    for c in mask[::-1]:
-        if c == 'e':
+def generate_noid(n, mask):
+    length = n
+    noid = ''
+    for char in mask[::-1]:
+        if char == 'e':
             div = len(XDIGIT)
-        elif c == 'd':
+        elif char == 'd':
             div = len(DIGIT)
         else:
             continue
         value = n % div
         n = n // div
-        xdig += (XDIGIT[value])
-        
+        noid += (XDIGIT[value])
+
     if mask[0] == 'z':
-        c = mask[1]
+        char = mask[1]
         while n > 0:
-            if c == 'e':
+            if char == 'e':
                 div = len(XDIGIT)
-            elif c == 'd':
+            elif char == 'd':
                 div = len(DIGIT)
             else:
-                raise InvalidTemplateError("Template mask is corrupt. Cannot process character: " + c)
+                raise InvalidTemplateError("Template mask is corrupt. Cannot process character: " + char)
             value = n % div
             n = n // div
-            xdig += (XDIGIT[value])
-        
+            noid += (XDIGIT[value])
+
     # if there is still something left over, we've exceeded our namespace. 
     # checks elsewhere should prevent this case from ever evaluating true.
     if n > 0:
-        raise NamespaceError("Cannot mint a noid for (counter = " + str(req) + ") within this namespace.")
-    
-    return xdig[::-1]
+        raise NamespaceError("Cannot mint a noid for (counter = " + str(length) + ") within this namespace.")
+
+    return noid[::-1]
 
 
-def __validateMask(mask):
-    masks = ['e', 'd']
-    checkchar = ['k']
+def _validate_mask(mask):
+    """Check to make sure that we have a valid mask
 
-    if not (mask[0] in GENTYPES or mask[0] in masks):
+    :param list mask: a sequence of characters
+    """
+    # check the first character
+    if not (mask[0] in GENTYPES or mask[0] in DIGTYPES):
         raise InvalidTemplateError("Template is invalid.")
-    elif not (mask[-1] in checkchar or mask[-1] in masks):
+    # check the last character
+    elif not (mask[-1] in CHECKDIG or mask[-1] in DIGTYPES):
         raise InvalidTemplateError("Template is invalid.")
+    # check all other characters
     else:
         for maskchar in mask[1:-1]:
-            if not (maskchar in masks):
+            if not (maskchar in DIGTYPES):
                 raise InvalidTemplateError("Template is invalid.")
 
-    return True         
+    return True
 
 
-def __getTotal(mask):
+def _get_total(mask):
     if mask[0] == 'z':
         total = NOLIMIT
     else:
@@ -128,31 +141,49 @@ def __getTotal(mask):
                 total *= len(XDIGIT)
             elif c == 'd':
                 total *= len(DIGIT)
-    return total            
-        
+    return total
 
-def __checkdigit(s):
+
+def calculate_check_digit(s):
+    """Given a string determine the check digit to be appended from the alphabet"""
     # TODO: Fix checkdigit to autostrip scheme names shorter or longer than 3 chars.
     try:
+        # if we have 'ark:/' remove them
         if s[3] == ':':
             s = s[4:].lstrip('/')
     except IndexError:
         pass
-    def ordinal(x):
-        try: return XDIGIT.index(x)
-        except: return 0
-    return XDIGIT[sum([x * (i+1) for i, x in enumerate(map(ordinal,s))]) % len(XDIGIT)]
+
+    def index_of(x):
+        """get the index of the extended digit"""
+        try:
+            return XDIGIT.index(x)
+        except:
+            return 0
+
+    total = list()
+    for i, x in enumerate(map(index_of, s)):
+        total += [x * (i + 1)]
+    index = sum([x * (i + 1) for i, x in enumerate(map(index_of, s))]) % len(XDIGIT)
+    return XDIGIT[index]
 
 
 class InvalidTemplateError(Exception):
     pass
 
+
 class ValidationError(Exception):
     pass
+
 
 class NamespaceError(Exception):
     pass
 
-if __name__ == '__main__':
 
+def main():
     print(mint(template='eeddeede', n=None, scheme='ark:/', naa='53696'))
+    return os.EX_OK
+
+
+if __name__ == '__main__':
+    sys.exit(main())
